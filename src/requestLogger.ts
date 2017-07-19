47dbed0c4ead972c90317558e91e75dd986c533f
momer <beancinematics@gmail.com>
2017-07-19 11:08:50 -0500

    createTarget implementation

--- src/requestLogger.ts
+++ src/requestLogger.ts
@@ -1,15 +1,21 @@
 /* eslint-disable import/prefer-default-export */
+const launchChrome = require('@serverless-chrome/lambda');
 const Cdp = require('chrome-remote-interface');
 const LOAD_TIMEOUT = 1000 * 30;
 
 export async function handler (event, context, callback) {
   const requestsMade = [];
 
-  const [tab] = await Cdp.List();
-  const client = await Cdp({ host: '127.0.0.1', target: tab });
+  const browser = await Cdp({ tab: 'ws://127.0.0.1:9222/devtools/browser' });
+	console.log("Browser connected...");
 
-  const { Network, Page } = client;
+	const newTarget = await browser.Target.createTarget({url: 'about:blank'});
+	const targetId = newTarget.targetId;
+  const targetList = await Cdp.List();
+	const url = targetList.find(target => target.id === targetId).webSocketDebuggerUrl;
+	const client = await Cdp({tab: url});
 
+	const { Emulation, Network, Page } = client;
   Network.requestWillBeSent(params => requestsMade.push(params));
 
   const loadEventFired = Page.loadEventFired();
@@ -32,13 +38,17 @@ export async function handler (event, context, callback) {
     );
 
     loadEventFired.then(async () => {
+			console.log("Load event fired...");
       clearTimeout(timeout);
       resolve();
     });
-  })
+  });
   // It's important that we close the websocket connection,
   // or our Lambda function will not exit properly
-  await client.close();
+
+	await client.close();
+	await browser.Target.closeTarget({targetId});
+	await browser.close();
 
   callback(null, {
     statusCode: 200,
